be7b957dccd17c780425f461d2106adc9b471bd5
mvolpi <matteo.volpi@epitech.eu>
2018-12-07 11:46:57 +0100

    database location updates

--- clear_db.py
+++ clear_db.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sqlite3 as sq
+import os
 
-conn = sq.connect("rfid_access.db")
+conn = sq.connect(os.getenv("HOME") + "/.rfid_access/rfid_access.db")
 curs = conn.cursor()
 ans = input("Delete users table? (y)").lower()
 if ans == "y":
